e5b72f81a04d1c4a5e7952cbbf41231e9f77c3b1
Martin Janser <martin@duss-janser.ch>
2016-03-14 21:50:11 +0100

    Fix test script

--- tests/test.sh
+++ tests/test.sh
@@ -4,8 +4,8 @@ ansible-playbook playbook.yml --syntax-check || exit 1
 
 ansible-playbook playbook.yml --connection=local || exit 1
 
-sleep 5
-curl -s -H "Host: example.com" https://localhost | grep -qi "cockpit" && echo "curl request was successful" || { echo "curl request failed" && exit 1; }
+sleep 2
+curl -s -H "Host: example.com" http://localhost | grep -qi "cockpit" && echo "curl request was successful" || { echo "curl request failed" && exit 1; }
 
 ansible-playbook playbook.yml --connection=local | tee /tmp/idempotence.log
 sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" /tmp/idempotence.log | grep -q "changed=0.*failed=0" \
